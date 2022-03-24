9db553d5fbfd6bc4d66b22e1013e57318b29404a
MichValenz <michvalenz27@gmail.com>
2022-03-24 13:46:31 -0500

    added screenshots of projects to projects component

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,53 +4,55 @@ import { Col, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 
 
+
 function Projects() {
 const projectList = [
   {
     name: "FiMi QA",
-    image: "../../images/FiMi.jpg",
+    image:  require("../../images/FiMi.png"),
     description:
       "An employee tracking application designed specifically for quality assurance departments.",
+    
     
   },
   {
     name: "Tech Cat",
-    image: "../../images/techcat.jpg",
+    image: require("../../images/techcat.jpeg"),
     description:
       "This application allows users to post and track tech related blogs.",
   },
   {
     name: "m5",
-    image: "../../images/m5.jpg",
+    image: require("../../images/m5.jpg"),
     description:
       "This application allows users to quickly find videos of searched actors.",
   },
   {
     name: "README Generator",
-    image: "../../images/readme.jpg",
+    image: require("../../images/readme.jpg"),
     description:
       "This backend only application creates README files for users with user input.",
   },
   {
     name: "note-taker",
-    image: "../../images/notetaker.jpg",
+    image: require("../../images/notetaker.jpg"),
     description:
       "From to-do lists to big ideas, Note Taker is an easy way to document and track your notes.",
   },
   {
     name: "run-buddy",
-    image: "../../images/runbuddy.jpg",
-    description: "A website that offers fitness training services..",
+    image: require("../../images/runbuddy.jpg"),
+    description: "A website that offers fitness training services.",
     
   },
 ];
 
- const render = (proj, index) => { 
+ const render = (proj) => { 
   return (
     <Row className="d-flex justify-content-around">
       <Col>
         <Card style={{ width: "18rem" }}>
-          <Card.Img key={index} variant="top" src={proj.image} />
+          <Card.Img  variant="top" src={proj.image} />
           <Card.Body>
             <Card.Title>{proj.name}</Card.Title>
             <Card.Text>{proj.description}</Card.Text>
