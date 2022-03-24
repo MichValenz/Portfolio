cf8fa8d1fba4927e9351a78ad0d7452ddc733cac
MichValenz <michvalenz27@gmail.com>
2022-03-23 22:43:28 -0500

    added cards to the projects file

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,59 +1,67 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { Col, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 
 function Projects() {
 const projectList = [
   {
     name: "FiMi QA",
+    image: "../../images/FiMi.jpg",
     description:
       "An employee tracking application designed specifically for quality assurance departments.",
     
   },
   {
     name: "Tech Cat",
+    image: "../../images/techcat.jpg",
     description:
       "This application allows users to post and track tech related blogs.",
   },
   {
     name: "m5",
+    image: "../../images/m5.jpg",
     description:
       "This application allows users to quickly find videos of searched actors.",
   },
   {
     name: "README Generator",
+    image: "../../images/readme.jpg",
     description:
       "This backend only application creates README files for users with user input.",
   },
   {
     name: "note-taker",
+    image: "../../images/notetaker.jpg",
     description:
       "From to-do lists to big ideas, Note Taker is an easy way to document and track your notes.",
   },
   {
     name: "run-buddy",
+    image: "../../images/runbuddy.jpg",
     description: "A website that offers fitness training services..",
+    
   },
 ];
-  return(
 
-<Row xs={1} md={2} className="g-4">
-  {Array.from({ length: 6 }).map((_, idx) => (
-    <Col>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
-  ))}
-</Row>);
+ const render = (proj, index) => { 
+  return (
+    <Row className="d-flex justify-content-around">
+      <Col>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img key={index} variant="top" src={proj.image} />
+          <Card.Body>
+            <Card.Title>{proj.name}</Card.Title>
+            <Card.Text>{proj.description}</Card.Text>
+            <Button variant="primary">Go to Website</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );}
+
+  return <div className="grid">{projectList.map(render)}</div>
 }
 
 export default Projects;
