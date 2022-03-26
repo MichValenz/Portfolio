0feef0392fb8d0a9ea16a33584cb85dac10359ca
MichValenz <michvalenz27@gmail.com>
2022-03-26 13:23:38 -0500

    updated about me page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,32 +6,61 @@ import headshot from "../../images/headshot.png"
 
 function About() {
   return (
-    <Container fluid bg-light>
-      <Row id="aboutMe">
-        <Col>
-          <h1 id="about">About Me</h1>
-          <p>
-            An analytically driven leader with 10 years of experience in
-            leadership, quality control, root cause analysis, error prevention,
-            technical writing, and cross-departmental collaboration. My
-            experiences as a stakeholder and as a user make me the perfect
-            addition to any web development project. I grew up in the outskirts
-            of a small town in west Texas with nothing to do and spotty wifi. I
-            spent my time surfing the web and it quickly became my safe space. I
-            know the importance of an efficient and engaging website.
-          </p>
-        </Col>
-        <Figure>
-          <Figure.Image
-            width={171}
-            height={180}
-            src={headshot}
-            className="img-fluid rounded-circle"
-            alt="headshot of Michelle Valenzuela"
-          />
-        </Figure>
-      </Row>
-    </Container>
+    <>
+      <Container fluid bg-light>
+        <Row id="aboutMe">
+          <Col>
+            <h1 id="about">About Me</h1>
+            <p>
+              I am a creative and solution driven web developer with a
+              background in project management, quality control, root cause
+              analysis, and cross- departmental collaboration. As a web
+              developer I have hours of experience with building full stack
+              applications. Because of my background in project management and
+              other roles, I understand what users and projects will require and
+              need from a full stack application. I am able to anticipate needs
+              and user desired experiences.
+            </p>
+
+            <p>
+              For example, my first major project was building the backend
+              routing and databases using MySQL, Sequelize, and Express.js for a
+              QA employee tracking application, but also leading my team’s scrum
+              meetings. My many years in QA allowed me to take lead and manage
+              the application’s initial planning, structuring, and organizing my
+              fellow developers goals. For new developers, organizing thoughts
+              and plans can be difficult, but I used my experience in management
+              to lead our team’s development plan. I helped the backend
+              developer plan and build the application’s models and assisted the
+              front-end developer with developing the user interface and tying
+              the front end to the backend code.
+            </p>
+
+            <p>
+              I am a highly adaptable, organized, supportive, and creative
+              professional that produces thoughtful and effective work. After
+              spending so many years on the other end of web development as
+              stakeholder, I am determined and excited to bring my experiences
+              and combine them with my creativity and technical skills to build
+              other project’s ideas to life. I am excited at the prospect of
+              helping others realize their ideas. I grew up in the outskirts of
+              a small town in west Texas with nothing to do, but use my spotty
+              wifi to browse the internet. I know the importance of an efficient
+              and engaging website.
+            </p>
+          </Col>
+          <Figure>
+            <Figure.Image
+              width={171}
+              height={180}
+              src={headshot}
+              className="img-fluid rounded-circle"
+              alt="headshot of Michelle Valenzuela"
+            />
+          </Figure>
+        </Row>
+      </Container>
+    </>
   );
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -24,7 +24,7 @@ function Navigation({currentOption, navOptionChange}) {
               href="/#About Me"
               onClick={() => navOptionChange("About Me")}
               className={
-                currentOption === "About Me" ? "currentOption" : "navActive"
+                currentOption === "About Me" ? "currentOption" : ""
               }
              
             >
@@ -64,74 +64,7 @@ function Navigation({currentOption, navOptionChange}) {
         </Nav>
       </header>
     </>
-    // <>
-    // <header className="flex-row px-1">
-    //   <h2>
-    //     <a
-    //       data-testid="link"
-    //       href="/"
-    //       onClick={() => navOptionChange("/")}
-    //       className={currentOption === "/" ? "currentOption" : ""}
-    //     >
-    //       Michelle Valenzuela
-    //     </a>
-    //   </h2>
-
-    //   <nav>
-    //     <ul className="flex-row">
-    //       <li className="mx-2">
-    //         <a
-    //           data-testid="link"
-    //           href="/"
-    //           onClick={() => navOptionChange("/")}
-    //           className={
-    //             currentOption === "Home" ? "main-nav-active" : "navActive"
-    //           }
-    //         >
-    //           Home
-    //         </a>
-    //       </li>
-    //       <li className="mx-2">
-    //         <a
-    //           data-testid="link"
-    //           href="#resume"
-    //           onClick={() => navOptionChange("Resume")}
-    //           className={
-    //             currentOption === "Resume" ? "main-nav-active" : "navActive"
-    //           }
-    //         >
-    //           Resume
-    //         </a>
-    //       </li>
-    //       <li className="mx-2">
-    //         <a
-    //           data-testid="link"
-    //           href="#Portfolio"
-    //           onClick={() => navOptionChange("Portfolio")}
-    //           className={
-    //             currentOption === "Portfolio" ? "main-nav-active" : "navActive"
-    //           }
-    //         >
-    //           Portfolio
-    //         </a>
-    //       </li>
-    //       <li className="mx-2">
-    //         <a
-    //           data-testid="link"
-    //           href="#contact"
-    //           onClick={() => navOptionChange("Contact")}
-    //           className={currentOption === "Contact" ? "currentOption" : ""}
-    //         >
-    //           Contact
-    //         </a>
-    //       </li>
-    //     </ul>
-    //   </nav>
-    // </header>
-    //    <div class="hero">
-    //   <div class="hero-section"></div>
-    // </div>
-    // </>
+    
   );
   
 }
